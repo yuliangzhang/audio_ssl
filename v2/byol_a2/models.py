662d5db7319b373dcf071f6a0a51be55b21566ac
yuliangzhang0030 <yuliangzhang0030@gmail.com>
2023-04-17 00:14:56 +0800

    202204162359: add tcn network

--- v2/byol_a2/models.py
+++ v2/byol_a2/models.py
@@ -13,6 +13,7 @@ import logging
 from pathlib import Path
 import torch
 from torch import nn
+from torch.nn.utils import weight_norm
 
 
 def load_pretrained_weights(model, pathname, model_key='model', strict=True):
@@ -102,3 +103,95 @@ class AudioNTT2022(AudioNTT2022Encoder):
         x = super().forward(x)
         x = mean_max_pooling(x)
         return x
+
+
+
+class Chomp1d(nn.Module):
+    def __init__(self, chomp_size):
+        super(Chomp1d, self).__init__()
+        self.chomp_size = chomp_size
+
+    def forward(self, x):
+        return x[:, :, :-self.chomp_size].contiguous()
+
+
+class TemporalBlock(nn.Module):
+    def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.2):
+        super(TemporalBlock, self).__init__()
+        self.conv1 = weight_norm(nn.Conv1d(n_inputs, n_outputs, kernel_size,
+                                           stride=stride, padding=padding, dilation=dilation))
+        self.chomp1 = Chomp1d(padding)
+        self.relu1 = nn.ReLU()
+        self.dropout1 = nn.Dropout(dropout)
+
+        self.conv2 = weight_norm(nn.Conv1d(n_outputs, n_outputs, kernel_size,
+                                           stride=stride, padding=padding, dilation=dilation))
+        self.chomp2 = Chomp1d(padding)
+        self.relu2 = nn.ReLU()
+        self.dropout2 = nn.Dropout(dropout)
+
+        self.net = nn.Sequential(self.conv1, self.chomp1, self.relu1, self.dropout1,
+                                 self.conv2, self.chomp2, self.relu2, self.dropout2)
+        self.downsample = nn.Conv1d(n_inputs, n_outputs, 1) if n_inputs != n_outputs else None
+        self.relu = nn.ReLU()
+        self.init_weights()
+
+    def init_weights(self):
+        self.conv1.weight.data.normal_(0, 0.01)
+        self.conv2.weight.data.normal_(0, 0.01)
+        if self.downsample is not None:
+            self.downsample.weight.data.normal_(0, 0.01)
+
+    def forward(self, x):
+        out = self.net(x)
+        res = x if self.downsample is None else self.downsample(x)
+        return self.relu(out + res)
+
+
+class TemporalConvNet(nn.Module):
+    def __init__(self, num_inputs, num_channels, kernel_size=2, dropout=0.2):
+        super(TemporalConvNet, self).__init__()
+        layers = []
+        num_levels = len(num_channels)
+        for i in range(num_levels):
+            dilation_size = 2 ** i
+            in_channels = num_inputs if i == 0 else num_channels[i-1]
+            out_channels = num_channels[i]
+            layers += [TemporalBlock(in_channels, out_channels, kernel_size, stride=1, dilation=dilation_size,
+                                     padding=(kernel_size-1) * dilation_size, dropout=dropout)]
+
+        self.network = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.network(x)
+
+
+class TCNModel(nn.Module):
+
+    def __init__(self, input_size, num_channels, kernel_size, dropout):
+        super(TCNModel, self).__init__()
+        self.tcn = TemporalConvNet(input_size, num_channels, kernel_size=kernel_size, dropout=dropout)
+
+    def forward(self, inputs):
+        # TCN model results
+        outputs = self.tcn(inputs)
+
+        # keep the time dimension inform
+
+        outputs = outputs.permute(0, 2, 1) # Batch,Time,Dimension
+
+        embedding = mean_max_pooling(outputs)
+
+        return embedding
+
+
+
+if __name__ == "__main__":
+
+    network = TCNModel(64, [32, 64, 128], 7, 0.05)
+
+    x = torch.randn(2, 64, 95)
+
+    res = network(x)
+
+    print(res.shape)
